e49018186635724030ef7f0371007720aaaf57d9
carlvin <calvinokumu@gmail.com>
2023-06-24 10:50:18 +0300

    small changes

--- carlhub.com/deploy/simple-crm-app/gunicorn/templates/gunicorn.conf.py
+++ carlhub.com/deploy/simple-crm-app/gunicorn/templates/gunicorn.conf.py
@@ -2,7 +2,9 @@ workers = 2
 syslog = True
 bind = ['127.0.0.1:8000']
 umask = 0
-loglevel ="info"
+errorlog = "home/xorb/logs/gunicorn.error"
+accesslog = "home/xorb/logs/gunicorn.access"
+loglevel ="debug"
 user = "xorb"
 group = "xorb"
 
